19e40e8599b794650874d02c359a1973a359118f
Dmitry Che <bediman@yandex.ru>
2023-12-13 21:16:25 +0300

    run test

---
+++ test.py
@@ -0,0 +1,28 @@
+/def run():
+    assert 2 == didfsfaf(10,5)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 7 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    assert 5 == fkoskf(10,2)
+    
+    
+    
+    
+    
+    
