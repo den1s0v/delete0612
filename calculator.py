52591e87a3ffde6c7ab74a9b1f9a08a2f3459ee7
deadlynight0 <152722063+deadlynight0@users.noreply.github.com>
2023-12-13 21:30:32 +0300

    изменения от Сергея (#1)
    ---------
    
    Co-authored-by: Dmitry Che <bediman@yandex.ru>

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
 def subtraction(a,b):
     """вычитание  a - b"""
     return a-b
+"""я хз что это"""
 
 def multiply(a, b):
 	""" Calculate A * B """
 	if b == 0:
 		return 0
 	return a * b
+"""Умножение двух чисел"""
 
 def divide(a, b):
     """ Calculate a / b """
     if b == 0:
         return 0
     return a / b
+"""Деление двух чисел"""
