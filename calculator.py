ed87ffbf3f8b3d13b36f84c9c9a64e9ace496d9e
Dmitry Che <bediman@yandex.ru>
2023-12-13 20:37:04 +0300

    Комментарии в калькулятор

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
 def subtraction(a,b):
     return a-b
+"""я хз что это"""
 
 def multiply(a, b):
 	if b == 0:
 		return 0
 	return a * b
+"""Умножение двух чисел"""
 
 def divide(a, b):
     if b == 0:
         return 0
     return a / b
+"""Деление двух чисел"""
